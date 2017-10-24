52646bbd22579e2e737e93b7a2e594c49098b869
Jack Coffey <15147827@studentmail.ul.ie>
2017-10-24 23:42:29 +0100

    ProfileDB is now a Singleton

--- CS4125CodeBase/src/Data/ProfileDB.java
+++ CS4125CodeBase/src/Data/ProfileDB.java
@@ -2,4 +2,14 @@ package Data;
 
 public class ProfileDB {
     
+    private static ProfileDB instance = null;
+    
+    public static ProfileDB getInstance()
+    {
+        if(instance == null)
+            instance = new ProfileDB();
+        
+        return instance;
+    }
+    
 }
