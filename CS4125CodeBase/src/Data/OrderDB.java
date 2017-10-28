091ff62c4a4928295e35f7cbcfd862fdf7fc8e5a
Jack Coffey <15147827@studentmail.ul.ie>
2017-10-28 15:21:23 +0100

    Fixed Orders.txt file reading bug

--- CS4125CodeBase/src/Data/OrderDB.java
+++ CS4125CodeBase/src/Data/OrderDB.java
@@ -63,12 +63,14 @@ public class OrderDB implements Database<Order>, Subject{
     @Override
     public void writeToFile(Order data)throws IOException
     {
-        File file = new File(".\\Resources\\Orders\\Orders.txt");
+        File file = new File(".\\src\\Resources\\Orders\\Orders.txt");
+        
         BufferedWriter writer = new BufferedWriter(new FileWriter(file));
         
         writer.append(data.toString());
         
         writer.close();
+
     }
     
 }
