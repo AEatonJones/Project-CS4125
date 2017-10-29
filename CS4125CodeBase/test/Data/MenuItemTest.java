c2569a64d83af0abdbd1ea06b02d734e7bc33841
Jack Coffey <15147827@studentmail.ul.ie>
2017-10-29 20:39:22 +0000

    Got MenuItemTest working
    Added JUnit and Hamcrest to library

--- CS4125CodeBase/test/Data/MenuItemTest.java
+++ CS4125CodeBase/test/Data/MenuItemTest.java
@@ -1,45 +1,25 @@
 package Data;
 
 import Business.Profiles.Cafe;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import java.io.IOException;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MenuItemTest {
 
-    private float cost;
-    String name;
-    Cafe cafe;
     
-    public MenuItemTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
-   
     @Test
     public void testGetCost() {
-        MenuItem item = new MenuItem(cafe,name,10.0f);
-        System.out.println("getCost");       
-        float expResult = 10.0f;
-        float result = item.getCost();
-        assertEquals(expResult, result, 10.0f);        
-        //fail("The test case is a prototype.");
+        Cafe cafe;
+        try
+        {
+            cafe = new Cafe("Cafe great", "101 avenue", 10, "", "");
+            MenuItem item = new MenuItem(cafe, "Scone",10.0f);
+            assertEquals(10.0f, item.getCost(), 0.0f);
+        } 
+        catch (IOException ex)
+        {
+            fail("Should not have thrown IOException");
+        }
     }   
 }
