2b4d99f068f4fa746ed2890e99409a44c51eb1e5
Jack Coffey <15147827@studentmail.ul.ie>
2017-11-05 18:32:28 +0000

    Added borders and spacing to the CustomerLoginUI

--- CS4125CodeBase/src/UI/CustomerLoginUI.java
+++ CS4125CodeBase/src/UI/CustomerLoginUI.java
@@ -11,27 +11,38 @@ public class CustomerLoginUI implements UI, ActionListener {
 
     @Override
     public void draw() {
-        frame = new JFrame("LOGIN");
+        frame = new JFrame("Login");
+        frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(300, 200);
-        frame.setLayout(new BorderLayout());
+        frame.setLayout(new GridLayout(1, 1));
         
+        //GridBagConstraints code found at https://stackoverflow.com/questions/29813566/how-do-i-create-spacing-in-between-jbuttons-with-gridbaglayout
+        GridBagConstraints bag = new GridBagConstraints();
+        bag.weightx = 1;
+        bag.weighty = 1;
+        bag.insets = new Insets(5, 0, 5, 0);
+        bag.gridwidth = GridBagConstraints.REMAINDER;
+        bag.fill = GridBagConstraints.BOTH;
+        
+        //BorderFactory code found at https://stackoverflow.com/questions/14117481/how-can-i-set-the-insets-of-a-jframe
         JPanel buttons = new JPanel();
-        buttons.setLayout(new BoxLayout(buttons, 3));
+        buttons.setBorder(BorderFactory.createEmptyBorder(10, 30, 10, 30));
+        buttons.setLayout(new GridBagLayout());
         
         signIn = new JButton("SIGN IN");
         signIn.addActionListener(this);
-        buttons.add(signIn);
+        buttons.add(signIn, bag);
         
         register = new JButton("REGISTER");
         register.addActionListener(this);
-        buttons.add(register);
+        buttons.add(register, bag);
         
         quit = new JButton("QUIT");
         quit.addActionListener(this);
-        buttons.add(quit);
+        buttons.add(quit, bag);
         
-        frame.add("Center", buttons);
+        frame.add(buttons);
              
         frame.setVisible(true);
     }
