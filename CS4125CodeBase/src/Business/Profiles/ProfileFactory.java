878cd951315a72a5f9fbf5c105222d2ad63396a6
Aaron Eaton Jones <15168786@studentmail.ul.ie>
2017-11-08 09:22:09 +0000

    STOP COMMITING AFTER EVERY SAVE :rage:
    :rage:

--- CS4125CodeBase/src/Business/Profiles/ProfileFactory.java
+++ CS4125CodeBase/src/Business/Profiles/ProfileFactory.java
@@ -22,14 +22,9 @@ public class ProfileFactory
     //public Customer(String firstname, String surname, String email, String password,String number,String type)
     private static Profile createCustomer(String[] array){
         Profile profile = null;
-        
         String firstname = null, surname = null, email = null, password = null, number = null;
-
-        String [] customerDetails = new String[5];
-        System.out.println("Begun creating profile");
-        
+        String [] customerDetails = new String[5];      
         profile = new Customer(firstname = array[0], surname = array[1], email = array[2], password = array[3], number = array[4]);
-        System.out.println("Finished creating profile");
         return profile;
     }
     
