3374a8a5d6f454e5906aaf1923339f32e6665790
HenryMaugh <15141608@studentmail.ul.ie>
2017-11-21 22:56:35 +0000

    one more thing before i go to bed :)

--- CS4125CodeBase/src/Business/Orders/ToGo.java
+++ CS4125CodeBase/src/Business/Orders/ToGo.java
@@ -3,14 +3,18 @@ import Data.MenuItem;
 
 public class ToGo extends Order{
 
-    
+    /**
+     * 
+     */
     public ToGo(MenuItem[] items, String paymentType){
         this.items = items;
         this.surcharge = 1.0f;
         this.baseEtf = 1;
         this.paymentType = paymentType;
     }
-    
+    /**
+     * 
+     */
     @Override
     public String getDetails(){
         String result = getCafe().getName() + ", ";
--- CS4125CodeBase/src/Business/Orders/ToStay.java
+++ CS4125CodeBase/src/Business/Orders/ToStay.java
@@ -3,14 +3,18 @@ import Data.MenuItem;
 
 public class ToStay extends Order{
 
-    
+    /**
+     * 
+     */
     public ToStay(MenuItem[] items, String paymentType){
         this.items = items;
         this.surcharge = 0.5f;
         this.baseEtf = 0;
         this.paymentType = paymentType;
     }
-    
+    /**
+     * 
+     */
     @Override
     public String getDetails(){
         String result = getCafe().getName() + ", ";
